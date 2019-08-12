572151fb8b37912c0cc174065e19034a3878fc07
Sam Harris VM <omegaonion@gmail.com>
2019-08-12 22:40:33 +0100

    completed fibonacci task

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    // not negative
+    if (n<0) return 'OOPS';
+    //base case
+    if (n === 1) return 1;
+    if (n===2) return 1;
+    return fibonacci(n-1) + fibonacci(n-2);
 }
 
 module.exports = fibonacci
