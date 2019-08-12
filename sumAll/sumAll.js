3be30d5e006f0b75739684ae837716c3cf6a3c61
Sam Harris VM <omegaonion@gmail.com>
2019-08-12 22:22:11 +0100

    sumAll task complete

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(x,y) {
+    // if NaN
+    if (!(Number.isInteger(x)) || !(Number.isInteger(y)) ) return 'ERROR';
+    // must be positive for some reason
+    if(x<0||y<0) return 'ERROR';
+    
+    // if x>y swap values
+    if (x>y) {
+        var temp = x;
+        x = y;
+        y = temp;
+    }
+    var total = y;
+    for (x; x <y;x++){
+        total+=x;
+    }
+    return total;
 }
 
 module.exports = sumAll
