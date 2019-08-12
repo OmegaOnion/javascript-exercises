eac8686caefc64955e1b2549e244019665c536fa
Sam Harris VM <omegaonion@gmail.com>
2019-08-12 22:29:57 +0100

    completed leapYears task

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    //divisble by 400 is leap year
+    if (year % 400 === 0) return true;
+    // divisble by 100 not a leap year
+    else if (year % 100 === 0) return false;
+    // divisible by 4 is leap year
+    else if (year % 4 === 0) return true;
+    else return false;
 }
 
 module.exports = leapYears
+
