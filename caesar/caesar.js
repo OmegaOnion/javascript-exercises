8fdeca2197d8bfe8a522af2316b099c5488c6664
Sam Harris <sam.harris@lifecycle-software.com>
2019-08-13 15:22:48 +0100

    failed attempt at casesar shift

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,27 @@
-const caesar = function() {
+const caesar = function(text,shift) {
 
+    var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
+'p','q','r','s','t','u','v','w','x','y','z'];
+    // for every character in text
+    for(i=0;i<text.length;i++){
+        // for every character in alphabet
+        for(j=0;j<alphabet.length;j++){
+            //if it a match
+            if(text.charAt(i)==alphabet[j]){
+                var shifted = j+shift; // character to set to
+            }
+        }
+        //swap
+        if (text.length>1){
+            text = text.substring(0,i-1) + alphabet[shifted] + text.substring[i+1,text.length];
+        } else {
+            text = alphabet[shifted];
+        }
+        
+        
+    } 
+    
+    return text;
 }
 
 module.exports = caesar
