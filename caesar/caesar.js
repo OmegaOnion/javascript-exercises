6b3700cd77cfd11335c63f35102ae456b15668f8
Sam Harris <sam.harris@lifecycle-software.com>
2019-08-13 16:27:24 +0100

    fixed some minor errors - now needs to retain case

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,24 +1,24 @@
 const caesar = function(text,shift) {
 
     var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
-'p','q','r','s','t','u','v','w','x','y','z'];
+    'p','q','r','s','t','u','v','w','x','y','z'];
+
+    var shifted = 0;
     // for every character in text
     for(i=0;i<text.length;i++){
         // for every character in alphabet
         for(j=0;j<alphabet.length;j++){
             //if it a match
-            if(text.charAt(i)==alphabet[j]){
-                var shifted = j+shift; // character to set to
+            if(text.charAt(i).toLowerCase()==alphabet[j]){
+                shifted = (j+shift); // character to set to          
             }
         }
         //swap
         if (text.length>1){
-            text = text.substring(0,i-1) + alphabet[shifted] + text.substring[i+1,text.length];
+            //text = text.substring(0,i-1) + alphabet[shifted] + text.substring(i+1,text.length);
         } else {
             text = alphabet[shifted];
         }
-        
-        
     } 
     
     return text;
