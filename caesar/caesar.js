0f2059a6f3c62f530630b2af94bbf14201a9ae23
Sam Harris VM <omegaonion@gmail.com>
2019-08-13 20:26:55 +0100

    completed caesar shift task

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -3,25 +3,48 @@ const caesar = function(text,shift) {
     var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
     'p','q','r','s','t','u','v','w','x','y','z'];
 
-    var shifted = 0;
+    var charList = [];
+
     // for every character in text
     for(i=0;i<text.length;i++){
-        // for every character in alphabet
-        for(j=0;j<alphabet.length;j++){
-            //if it a match
-            if(text.charAt(i).toLowerCase()==alphabet[j]){
-                shifted = (j+shift); // character to set to          
+        // check alpha
+        if (!isLetter(text.charAt(i))){
+            charList[charList.length] = text.charAt(i);
+        } else { // is alpha
+            // preserve case
+            var upper = false;
+            if (text.charAt(i).toUpperCase() === text.charAt(i)) upper = true;
+            var shifted = 0;
+            // for every character in alphabet
+            for(j=0;j<alphabet.length;j++){
+                //if it a match
+                if(text.charAt(i).toLowerCase()==alphabet[j]){
+                    shifted = (j+shift); // character to set to          
+                }
             }
+            //if out upper bounds
+            while (shifted >= alphabet.length) shifted -=alphabet.length;
+            //if out lower bounds
+            while (shifted < 0) shifted +=alphabet.length;
+            // new character
+            var newChar = alphabet[shifted];
+            if (upper) newChar = newChar.toUpperCase();
+         
+            charList[charList.length] = newChar;            
         }
-        //swap
-        if (text.length>1){
-            //text = text.substring(0,i-1) + alphabet[shifted] + text.substring(i+1,text.length);
-        } else {
-            text = alphabet[shifted];
-        }
+
+    }
+
+    text  = ""
+    for (i = 0; i<charList.length;i++){
+        text+=charList[i];
     } 
     
     return text;
 }
+
+function isLetter(str) {
+    return str.length === 1 && str.match(/[a-z]/i);
+  }
 
 module.exports = caesar
