dbf98d9a983ab3be63889327f650bf9844b43c84
Sam Harris <sam.harris@lifecycle-software.com>
2019-09-04 11:50:24 +0100

    completed find the oldest task

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
+    return people.reduce(oldestPerson);
+}
 
+function oldestPerson(oldest,currentPerson){
+    currentOldestAge = calcAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    currentAge = calcAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+    return currentAge > currentOldestAge ? currentPerson : oldest;
+}
+
+function calcAge(birth, death){
+    death = typeof death == "undefined" ? new Date().getFullYear() :  death;
+    return death-birth;
 }
 
 module.exports = findTheOldest
